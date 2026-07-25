42f7cc84cf38bd64033ff1c6310b957bd8a8e0c4
tiktiki <itzpomodoro79@gmail.com>
2026-07-25 21:46:04 +0600

    Initital commit

---
+++ FIrst Contest/My solution/problem_1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string line;
+    cout << "Enter a line: ";
+    getline(cin, line);
+
+    stringstream ss(line);
+    string word;
+
+    while(ss >> word){
+        for(char &c: word){
+            c = toupper(c);
+        }
+        cout << word << endl;
+    }
+
+
+    return 0;
+}
