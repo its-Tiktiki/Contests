1402b04a25dd31c3b9559474e4fda2145770001c
tiktiki <itzpomodoro79@gmail.com>
2026-07-25 21:49:46 +0600

    add solution of problem 4

---
+++ FIrst Contest/My solution/problem_4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cout << "Enter test case number: ";
+	cin >> t;
+	while(t--){
+		long long N;
+		cin >> N;
+		cout << N*4LL << endl;
+	}
+    return 0;
+}
